c5421204443da094751ab41c85cebe3c00d5e401
StupidUsernamee <amirm.mamnoon@gmail.com>
2025-08-31 21:39:06 +0330

    env module added and .gitignore updated
    env module load env variables

---
+++ env/env.py
@@ -0,0 +1,54 @@
+import os
+import logging
+
+from dotenv import load_dotenv
+
+
+logging.basicConfig(level=logging.INFO)
+
+class GetEnv():
+
+    def __init__(self):
+        load_dotenv()
+
+    def get_str(self, key: str, fallback: str) -> str:
+        val = os.getenv(key)
+
+        if val is None:
+            logging.warning(f"env[{key}] not found, using fallback value: {fallback}")
+            return fallback
+        
+        logging.info(f"key[{key}]: {val}")
+        return val
+
+
+    def get_int(self, key: str, fallback: int) -> int:
+        val = os.getenv(key)
+
+        if val is None:
+            logging.warning(f"env[{key}] not found, using fallback value: {fallback}")
+            return fallback
+
+        try:
+            return int(val)
+        except ValueError:
+            logging.warning(f"env[{key}] invalid value, has to be int, using fallback value: {fallback}")
+            return fallback
+
+    
+    def get_bool(self, key: str, fallback: bool) -> bool:
+        val = os.getenv(key)
+
+        if val is None:
+            logging.warning(f"env[{key}] not found, using fallback: {fallback}")
+            return fallback
+        
+        if val.lower() in ("yes", "1", "true"):
+            return True
+
+        if val.lower() in ("no", "0", "false"):
+            return False
+        
+        logging.warning(f"env[{key}] invalid value, has to be bool, using fallback value: {fallback}")
+        return fallback
+
