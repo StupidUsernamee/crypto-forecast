551f85a08999b27c506c006d50989a04a7d762d6
StupidUsernamee <amirm.mamnoon@gmail.com>
2025-09-01 13:19:54 +0330

    Overall chage in load_to_lake
    Now, it saves data into a jsonl file.
    jsonl files that each save hourly data

--- etl/extract/load_to_lake.py
+++ etl/extract/load_to_lake.py
@@ -1,13 +1,25 @@
 from .fetch_data import read_api
+from env.env import GetEnv
 
+import os
 import json
 from typing import Any
+from datetime import datetime, timezone
 
-def load_data(path: str, url: str, params: dict[str, Any]):
-    
-    data_to_load = read_api(url=url, params=params)
+getEnv = GetEnv()
+DATA_DIR = getEnv.get_str("DATA_LAKE_PATH", "./data/raw/BTC/")
 
-    with open(path, "a", encoding="utf-8") as file:
-        json.dump(data_to_load, file)
-        file.write("\n")
-        print("data saved to file")
+os.makedirs(DATA_DIR, exist_ok=True)
+
+def get_file_path():
+    now = datetime.now(timezone.utc)
+    return os.path.join(
+        DATA_DIR,
+        now.strftime("%Y-%m-%d-%H") + ".jsonl"
+    )
+
+
+def append_record(record: dict[str, Any]):
+    path = get_file_path()
+    with open(path, 'a') as f:
+        f.write(json.dumps(record) + "\n")
