5557d6c2ed2c64a3151c985993b60834afb5f30f
StupidUsernamee <amirm.mamnoon@gmail.com>
2025-09-01 13:18:51 +0330

    Chages in return value of read_api function
    Now, it returns status_code alongside with data itself.

--- etl/extract/fetch_data.py
+++ etl/extract/fetch_data.py
@@ -3,14 +3,14 @@ import json
 from typing import Any
 
 
-def read_api(url: str, params: dict) -> dict[str, Any]:
+def read_api(url: str, params: dict) -> tuple[dict[str, Any], int]:
     try: 
         resp = requests.get(url=url, params=params)
 
         data = resp.json()
 
-        return data
+        return (data, resp.status_code)
 
     except json.JSONDecodeError as e:
         print(f"Cannot decode, err: {e}")
-        return dict()
+        return (dict(), 403)
